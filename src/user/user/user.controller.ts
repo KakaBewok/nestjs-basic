7bd6108af9f38c30c5f1c215063343258d921ce1
noprizal <noprizal@voltras.co.id>
2024-03-13 14:15:31 +0700

    decorator

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Param, Post, Query, Redirect, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
 
+    //response
+    @Get("/sample-response")
+    @Header("Content-Type", "application/json")
+    @HttpCode(200)
+    getSampleResponse(): Record<string, string> {
+        return {
+            "data" : "Hello json"
+        };
+    }
+    //response redirect
+    @Get("/redirect")
+    @Redirect()
+    redirect() : HttpRedirectResponse {
+        return {
+            url: "/api/users/sample-response",
+            statusCode: 301,  
+        }
+    }
+
+    //query
     @Get("/hello")
     getByIdQuery(
         @Query("first_name") firstName: string,
@@ -12,6 +32,7 @@ export class UserController {
         return `Hello ${firstName || ''} ${lastName || ''}`;
     }
 
+    //param
     @Get("/:id")
     getByIdParam(@Param("id") id: string): string {
         return `get param ${id}`;
